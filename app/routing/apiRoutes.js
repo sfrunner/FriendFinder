6ee86cb693e08aca88ef6ccc23deb302369dbb7d
Steven Fox <sfox@art.com>
2017-04-30 16:06:27 -0700

    App is Functioning at 100%

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 module.exports = function(app,path){
 var friendsArray = [];
 var newFriendFinalValue = 0;
+var pastUserScore = 0;
 var lowestValue;
 var recommendedFriend;
 
@@ -11,21 +12,28 @@ app.get("/api/friends", function(req, res){
 
 app.post("/api/friends", function(req, res){
     newFriend = req.body;
+    console.log(newFriend);
     
     //Sum up Values
-    for(var a = 0; a < req.body.values; a++){
-        newFriendFinalValue += parseInt(req.body.value[i]);
+    for(var a = 0; a < newFriend.value.length; a++){
+        console.log(req.body.value[a].value);
+        newFriendFinalValue += parseInt(newFriend.value[a].value);
     }
    if(friendsArray.length >= 1){
     for(var i = 0; i < friendsArray.length; i++){
-        var pastUserScore = parseInt(friendsArray[i].finalScore);
+        for(var b = 0; b < friendsArray[i].value.length; b++){
+            pastUserScore += parseInt(friendsArray[i].value[b].value);
+        }
         var differenceValue = Math.abs(newFriendFinalValue - pastUserScore);
         if(differenceValue <= lowestValue || lowestValue == null){
             lowestValue = differenceValue;
             recommendedFriend = friendsArray[i];
         }
+        pastUserScore = 0;
 }
     console.log(recommendedFriend.name);
+    console.log(newFriendFinalValue);
+    console.log(lowestValue);
     res.send(recommendedFriend);
    }
    else{
