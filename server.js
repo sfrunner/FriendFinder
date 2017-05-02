937fab8ce71af3aa841796ce6cfd08d368f44666
Steven Fox <sfox@art.com>
2017-05-01 21:46:36 -0700

    Heroku Changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var path = require("path");
+var PORT = process.env.PORT || 8080;
 app.use(express.static(path.join( __dirname,"app")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,4 +11,4 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("./app/routing/apiRoutes.js")(app,path);
 require("./app/routing/htmlRoutes.js")(app,path);
 
-app.listen(3080, "localhost");
+app.listen(PORT, "localhost");
